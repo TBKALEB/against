1a913791a232dd03a8a779f7d25e66e0b99b586d
TBKaleb <kalebjesusuno1@gmail.com>
2025-09-15 22:29:49 -0600

    Primer commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,41 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Head from "next/head";
+import logo from "../../public/logo.png";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ Component, pageProps }) {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.container}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={styles.info}>
+        <div className={styles.title_conta}>
+          <h1>ADVERTENCIA!!!</h1>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.sub_conta}>
+          <h2>VOTOS POR PARTE DEL PARTIDO VERDE HAN SIDO ROBADOS</h2>
+        </div>
+        <div className={styles.image_conta}>
+          <Link
+            href="https://www.instagram.com/somos_prepatala/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={logo} width={300} height={300} alt="logo" />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.fraude_conta}>
+        <h1>Ha habido un fraude por su parte</h1>
+      </div>
+      <div className={styles.footer_conta}>
+        <h2>
+          "No estamos a favor de nadie, pero no queremos nuestra prepa pintada
+          de verde."
+        </h2>
+      </div>
     </div>
   );
 }
